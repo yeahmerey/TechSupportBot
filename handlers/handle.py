ac17441b328d62a35a60983a19c394ccc2d371fd
Merey Kaliyev <yeahmerey@mail.ru>
2025-03-09 13:11:35 +0500

    learn {fsm context} : registration

--- handlers/handle.py
+++ handlers/handle.py
@@ -1,10 +1,16 @@
 from aiogram import F, Router
 from aiogram.filters import CommandStart , Command
 from aiogram.types import Message , CallbackQuery
+from aiogram.fsm.state import StatesGroup, State 
+from aiogram.fsm.context import FSMContext
 
 import keyboards.keyboard as kb ; 
 
 router = Router()
+
+class Reg(StatesGroup):
+    name = State()
+    number = State()
 
 @router.message(CommandStart())
 async def cmd_start(message : Message) :
@@ -30,3 +36,23 @@ async def basket(callback : CallbackQuery):
     await callback.message.edit_text('Hello', reply_markup=await kb.inline_cars())
 
     #edit-text to edit-caption if image
+
+@router.message(Command('reg'))
+async def reg_first_step(message : Message, state : FSMContext):
+    await state.set_state(Reg.name)
+    await message.answer('Введите ваше имя:')
+
+@router.message(Reg.name)
+async def reg_second_step(message : Message , state : FSMContext):
+    await state.update_data(name=message.text)
+    await state.set_state(Reg.number)
+    await message.answer('Введите номер телефона')
+
+@router.message(Reg.number)
+async def reg_third_step(message : Message , state : FSMContext):
+    await state.update_data(number = message.text)
+    data = await state.get_data()
+    await message.answer(f'Salem , Reg ushin rahmet : \nName:{data["name"]}\nNumber:{data["number"]}')
+    await state.clear()
+
+    
